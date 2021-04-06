6d2da31de0486698a59b1ed8a54c17909ce21cdb
osabdelkhaleq@gmail.com <osabdelkhaleq@gmail.com>
2021-04-06 14:53:05 +0000

    Week 2

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -3,7 +3,7 @@ select
     customers.customer_id,
     coalesce(payments.amount,0) as amount
 from {{ ref('stg_orders')}} orders
-left outer join {{ ref('dim_customers')}} customers
+left outer join {{ ref('stg_customers')}} customers
   on customers.customer_id = orders.customer_id
 left outer join {{ ref('stg_payments')}} payments
   on orders.order_id = payments.order_id
